16aa3d9dc60c8dc64466f53a995473e9d8c984ca
YOT-ANAN <5910110270@psu.ac.th>
2018-09-26 01:45:55 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const routerOptions = [
+  {
+    path: '/signup',
+    component: 'SignUp'
+  },
+  {
+    path:  '/signin',
+    component: 'Login'
+  },
+  {
+    path: '/',
+    component: 'Login'
+  }
+]
+
+const routes = routerOptions.map(route => {
+  return {
+    path: route.path,
+    component: () => import (`@/components/${route.component}.vue`)
+  }
+})
+
+export default new Router({
+  mode: 'history',
+  routes
+})
